569c5da5d47627a01d4d2a7e8493e30b0833fd85
natyvit <natalia16.nv@gmail.com>
2022-11-15 12:18:00 -0300

    função de galeria para bicicletas

--- js/script.js
+++ js/script.js
@@ -41,3 +41,21 @@ function eventosPerguntas(pergunta){
 }
 
 perguntas.forEach(eventosPerguntas)
+
+// Galeria de bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imgs img')
+const galeriaContainer = document.querySelector('.bicicleta-imgs')
+
+function trocarImg(event){
+  const img = event.currentTarget
+  const media = window.matchMedia('(min-width: 1000px)').matches
+  if(media){
+    galeriaContainer.prepend(img)
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarImg )
+}
+
+galeria.forEach(eventosGaleria)
